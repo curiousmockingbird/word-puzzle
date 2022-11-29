ddd7db3cb2b70e5b37692226ed042267274940fa
Harold_Mesa <harold.mesa@icloud.com>
2022-11-29 15:49:24 -0600

    Pass 2nd test for main-site-reducer.

--- src/reducers/main-site-reducer.js
+++ src/reducers/main-site-reducer.js
@@ -1,5 +1,16 @@
 const reducer = (state = {}, action) => {
- return state;
+  const { word, id } = action;
+  switch (action.type) {
+    case 'CREATE_WORD':
+      return Object.assign ({}, state, {
+        [id]: {
+          word: word,
+          id: id
+        }
+      })
+default:
+    return state;
+  }
 };
 
 export default reducer;
