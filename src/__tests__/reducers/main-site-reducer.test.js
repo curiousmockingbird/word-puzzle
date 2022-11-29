93a115ec1c95db37583c046dd7b7ad0f2f88f9e1
Harold_Mesa <harold.mesa@icloud.com>
2022-11-29 15:42:05 -0600

    Add 2nd test for main-site-reducer.js

--- src/__tests__/reducers/main-site-reducer.test.js
+++ src/__tests__/reducers/main-site-reducer.test.js
@@ -1,7 +1,28 @@
 import mainSiteReducer from "../../reducers/main-site-reducer";
 
 describe('mainSiteReducer', () => {
+  let action;
+  const wordToGuess = {
+    word: 'random',
+    id: 1
+  }  
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(mainSiteReducer({}, { type: null})).toEqual({});
   });
+
+  test('Should successfully create word to guess', () => {
+    const { word, id } = wordToGuess;
+    action = {
+      type: 'CREATE_WORD',
+      word: word,
+      id: id
+    };
+
+    expect(mainSiteReducer({}, action)).toEqual({
+      [id]: {
+        word: word,
+        id: id
+      }
+    })
+  })
 })
